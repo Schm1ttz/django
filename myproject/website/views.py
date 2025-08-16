c1cf47c03e7bf37394d96565ba786a551d9377a0
Schm1ttz <davikaspary@gmail.com>
2025-08-16 11:47:35 -0300

    Add files via upload

---
+++ myproject/website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'website/home.html')
+
+def sobre(request):
+    return render(request, 'website/sobre.html')
+
+def contato(request):
+    return render(request, 'website/contato.html')
+
+# Create your views here.
